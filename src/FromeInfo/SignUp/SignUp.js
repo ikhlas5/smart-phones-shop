d98d6e7c8c04546ca1ce70f59efda69b0a0d987d
ikhlas5 <mohammadikhlas765@gmail.com>
2022-12-02 20:35:14 +0600

    13 commit

--- src/FromeInfo/SignUp/SignUp.js
+++ src/FromeInfo/SignUp/SignUp.js
@@ -89,6 +89,7 @@ const SignUp = () => {
                 <form onSubmit={handleSubmit(handleSignUp)}>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"> <span className="label-text">Name</span></label>
+                        
                         <input type="text" {...register("name", {
                             required: "Name is Required"
                         })} className="input input-bordered w-full max-w-xs" />
